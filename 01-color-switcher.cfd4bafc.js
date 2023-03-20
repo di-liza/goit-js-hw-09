5badb89470e89dd91054ca7950ab5277806109f5
di-liza <114363326+di-liza@users.noreply.github.com>
2023-03-20 20:47:59 +0000

    Deploying to gh-pages from @ di-liza/goit-js-hw-09@96e142b465c512a6f29dfc1f7f08bef5ba917615 🚀

---
+++ 01-color-switcher.cfd4bafc.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop");t.disabled=!0;let d=null,a=!1;e.addEventListener("click",(()=>{a=!0,a&&(e.disabled=!0,t.disabled=!1),d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(()=>{clearInterval(d),t.disabled=!0,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.cfd4bafc.js.map
